de18c9df871b88d2194d1e356efae2f8e0ae60f8
yunchao_liu <yunchao_liu@>
2019-09-04 18:33:57 +0000

    bug: failed to add user to existing group

--- cmd/smallpoint/handlers.go
+++ cmd/smallpoint/handlers.go
@@ -927,7 +927,7 @@ func (state *RuntimeState) addmemberstoExistingGroup(w http.ResponseWriter, r *h
 		groupinfo.MemberUid = append(groupinfo.MemberUid, member)
 	}
 
-	if len(groupinfo.Member) > 0 {
+	if len(groupinfo.MemberUid) > 0 {
 		err = state.Userinfo.AddmemberstoExisting(groupinfo)
 		if err != nil {
 			log.Println(err)
