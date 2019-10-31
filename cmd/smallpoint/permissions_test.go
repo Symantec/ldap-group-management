b8afc2e6d075d67f7627f1efab1c11dd1ac1e933
yunchao_liu <yunchao_liu@>
2019-10-31 00:01:13 +0000

    fix build fail

--- cmd/smallpoint/permissions_test.go
+++ cmd/smallpoint/permissions_test.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"log"
 	"testing"
 )
 
@@ -14,34 +13,6 @@ type resourcePerm struct {
 	resourceType int
 	resource     string
 	permission   int
-}
-
-func mockPermissionDB(state RuntimeState) error {
-	_, err := state.db.Exec(`delete from permissions`)
-	if err != nil {
-		return err
-	}
-	var insertStmt = `insert  into permissions(groupname, resource_type, resource, permission) values (?,?,?,?);`
-	stmt, err := state.db.Prepare(insertStmt)
-	if err != nil {
-		log.Print("Error preparing statement" + insertStmt)
-		log.Fatal(err)
-		return err
-	}
-	defer stmt.Close()
-	_, err = stmt.Exec("group2", resourceGroup, "group1", permDelete|permCreate)
-	if err != nil {
-		return err
-	}
-	_, err = stmt.Exec("group2", resourceSVC, "new_svc_account", permCreate)
-	if err != nil {
-		return err
-	}
-	_, err = stmt.Exec("group2", resourceGroup, "foo", permCreate)
-	if err != nil {
-		return err
-	}
-	return nil
 }
 
 var resourcePermList = []resourcePerm{
@@ -57,10 +28,6 @@ func TestCanPerformAction(t *testing.T) {
 		t.Fatal(err)
 	}
 
-	err = mockPermissionDB(state)
-	if err != nil {
-		t.Fatal(err)
-	}
 	for _, item := range resourcePermList {
 		allow, err := state.canPerformAction(userWithPerm, item.resource, item.resourceType, item.permission)
 		if err != nil {
