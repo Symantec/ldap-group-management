d56aa0ef3f2b34d26da4c542c944dfe0e7bb5438
yunchao_liu <yunchao_liu@>
2019-11-07 22:44:16 +0000

    add test function for check match

--- cmd/smallpoint/permissions_test.go
+++ cmd/smallpoint/permissions_test.go
@@ -7,7 +7,11 @@ import (
 const (
 	userWithPerm    = "user3"
 	userWithoutPerm = "user2"
+	input           = "group3"
 )
+
+var resourcesAllow = []string{"*", "group3*"}
+var resourcesDeny = []string{"group2", "group33"}
 
 type resourcePerm struct {
 	resourceType int
@@ -46,4 +50,26 @@ func TestCanPerformAction(t *testing.T) {
 		}
 	}
 
+}
+
+func TestCheckResourceMatch(t *testing.T) {
+	for _, resource := range resourcesAllow {
+		match, err := checkResourceMatch(resource, input)
+		if err != nil {
+			t.Fatal(err)
+		}
+		if match != true {
+			t.Errorf("%v got %v want true", resource, match)
+		}
+	}
+
+	for _, resource := range resourcesDeny {
+		match, err := checkResourceMatch(resource, input)
+		if err != nil {
+			t.Fatal(err)
+		}
+		if match != false {
+			t.Errorf("%v got %v want true", resource, match)
+		}
+	}
 }
