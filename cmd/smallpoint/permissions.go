3ec2c0016fcb81647f2baea7f837d94c674504e5
yunchao_liu <yunchao_liu@>
2019-11-07 00:33:45 +0000

    get groupname and resource at the same time

--- cmd/smallpoint/permissions.go
+++ cmd/smallpoint/permissions.go
@@ -7,8 +7,8 @@ import (
 )
 
 var checkPermissionStmt = map[string]string{
-	"sqlite":   "select groupname from permissions where (resource=? or resource='*' or resource LIKE ?) and resource_type=? and (permission&?=?);",
-	"postgres": "select groupname from permissions where (resource=$1 or resource='*' or resource LIKE $2) and resource_type=$3 and (permission&$4=$5);",
+	"sqlite":   "select groupname, resource from permissions where (resource=? or resource='*' or resource LIKE ?) and resource_type=? and (permission&?=?);",
+	"postgres": "select groupname, resource from permissions where (resource=$1 or resource='*' or resource LIKE $2) and resource_type=$3 and (permission&$4=$5);",
 }
 
 var checkResourceStmt = map[string]string{
@@ -27,47 +27,18 @@ const (
 	resourceSVC
 )
 
-func checkResourceMatch(groupname, input string, resource_type, permission int, state *RuntimeState) (bool, error) {
-	stmtText := checkResourceStmt[state.dbType]
-	stmt, err := state.db.Prepare(stmtText)
-	if err != nil {
-		log.Println("Error prepare statement " + stmtText)
-		return false, err
-	}
-	defer stmt.Close()
-
-	rows, err := stmt.Query(groupname, resource_type, permission, permission)
-	if err != nil {
-		if err.Error() == "sql: no rows in result set" {
-			log.Println("No rows found")
-			return false, err
-		} else {
-			log.Println(err)
-			return false, err
-
-		}
-	}
-	defer rows.Close()
-
-	for rows.Next() {
-		var target string
-		err = rows.Scan(&target)
-		if err != nil {
-			log.Println(err)
-			return false, err
-		}
-		if strings.HasSuffix(target, "*") {
-			if len(target) == 1 {
-				return true, nil
-			}
-			target = target[:len(target)-1]
-			if strings.HasPrefix(input, target) {
-				return true, nil
-			}
-		}
-		if target == input {
+func checkResourceMatch(resource, input string) (bool, error) {
+	if strings.HasSuffix(resource, "*") {
+		if len(resource) == 1 {
 			return true, nil
 		}
+		resource = resource[:len(resource)-1]
+		if strings.HasPrefix(input, resource) {
+			return true, nil
+		}
+	}
+	if resource == input {
+		return true, nil
 	}
 	return false, nil
 }
@@ -99,12 +70,13 @@ func getPermittedGroups(resources string, resource_type, permission int, state *
 
 	for rows.Next() {
 		var groupName string
-		err = rows.Scan(&groupName)
+		var resourceName string
+		err = rows.Scan(&groupName, &resourceName)
 		if err != nil {
 			log.Println(err)
 			return nil, err
 		}
-		match, err := checkResourceMatch(groupName, resources, resource_type, permission, state)
+		match, err := checkResourceMatch(resourceName, resources)
 		if err != nil {
 			log.Println(err)
 			return nil, err
@@ -122,6 +94,7 @@ func (state *RuntimeState) canPerformAction(username, resources string, resource
 	if err != nil {
 		return false, err
 	}
+
 	if len(groups) < 1 {
 		return false, nil
 	}
@@ -134,7 +107,6 @@ func (state *RuntimeState) canPerformAction(username, resources string, resource
 	if state.Userinfo.UserisadminOrNot(username) {
 		return true, nil
 	}
-
 	for _, group := range groups {
 		var index int
 		index = sort.SearchStrings(groupsOfUser, group)
